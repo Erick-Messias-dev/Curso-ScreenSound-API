e97709d62ec4ed60236d8e979ea0603084297ea9
Erick-Messias-dev <erick.messias@outolook.com.br>
2025-07-23 14:22:23 -0300

    primeiro commit

---
+++ ScreenSound-API/Program.cs
@@ -0,0 +1,20 @@
+﻿using ScreenSound_API.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        musicas[0].ExibirFichaTecnica();
+    }
+    catch(Exception ex) 
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+    
+    
+}
