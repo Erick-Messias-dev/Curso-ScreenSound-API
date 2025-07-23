87d33bd1eaafba3af0f54d4707341e3ea29ebe5c
Erick-Messias-dev <erick.messias@outolook.com.br>
2025-07-23 15:24:46 -0300

    adicionando linq filters

---
+++ ScreenSound-API/Filtros/LinqFilter.cs
@@ -0,0 +1,36 @@
+﻿using ScreenSound_API.Modelos;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ScreenSound_API.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    { 
+        var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+
+        foreach (var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+
+   public static void FiltrarArtistasPorGeneroMusical(List<Musica> musicas, string genero)
+    {
+        var artistasPorGeneroMusical = musicas.Where(musica => musica.Genero.Contains(genero)).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine($"Exibindo os artistas por gênero musical >>> {genero}");
+        foreach(var artista in artistasPorGeneroMusical)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+
+    public static void FiltrarMusicasDeUmArtista(List<Musica> musicas, string nomeArtista)
+    {
+        var musicasDoArtista = musicas.Where(musica => musica.Artista!.Equals(nomeArtista)).ToList();
+        foreach (var musica in musicasDoArtista)
+        {
+            Console.WriteLine($"- {musica.Nome}");
+        }
+    }
+}
